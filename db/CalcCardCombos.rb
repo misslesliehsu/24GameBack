1d450c86bba99c5e2e164d210d8f44b62aed33b9
Flatiron School <leslie.hsu@flatironschool.com>
2018-04-12 22:21:58 -0400

    testing in progress

--- db/CalcCardCombos.rb
+++ db/CalcCardCombos.rb
@@ -1,10 +1,15 @@
 all = ["+", "-", "%", "*", "(", ")", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-syms = ["+", "-", "/", "*", "(", ")", " "]
+syms = ["+", "-", "/", "*"]
+spsyms = [" ", "(", ")"]
 
-tester = [nums, syms, nums, syms, nums, syms, nums, syms]
+# tester1 = [nums, syms, nums, syms, nums, syms, nums, syms] (where syms were all symbols inluding ())
+# tester2 = [spsyms, nums, spsyms, syms, spsyms, nums, spsyms, syms, spsyms, nums, spsyms, syms, spsyms, nums, spsyms, syms, spsyms]
 
+tester3pt1 = [spsyms, nums, syms, nums, spsyms, nums, syms, nums]
+tester3pt2 = [spsyms, nums, syms, nums, syms, nums, spsyms, nums]
+tester3pt3 = [spsyms, nums, syms, nums, spsyms, spsyms, nums, syms, nums, spsyms]
 
 def permutations(arrays, i)
   if i == arrays.length then return [[]] end
@@ -19,8 +24,7 @@ def permutations(arrays, i)
     return res
 end
 
-all_results = permutations(tester, 0)
-
+all_results = permutations(tester3pt1, 0)
 
 cards = []
 
@@ -39,16 +43,26 @@ cards.each do |array|
   numsOnly = array.select do |char|
     !syms.include?(char)
   end
+  #
+  # if numsOnly.include?("6" && "4") && numsOnly.uniq.length == 2
+  #   print array
+  #   debugger
+  # end
+  #
+  # if numsOnly.include?("8" && "3" && "4") && numsOnly.uniq.length == 3
+  #   print array
+  # end
+
   candidate = numsOnly.map do |num|
     num.to_i
   end
+  #
+  # if candidate == [6, 4, 4, 6] then debugger end
+  # if candidate == [8, 8, 3, 4] then debugger end
+
   somefile.print candidate
   somefile.print ","
 end
-
-# cards.each do |array|
-#   somefile.print array
-# end
 
 
 somefile.close
